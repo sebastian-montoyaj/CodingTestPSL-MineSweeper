c73ebbeed788198f7c777e52bc8a88fcc76ce745
Sebastián Montoya Jiménez <yatocapo@gmail.com>
2018-09-02 09:58:24 -0500

    Finished Tile Testing
    I implemented the basic tests for Tile class.

--- test/co/edu/udea/game/GridTest.java
+++ test/co/edu/udea/game/GridTest.java
@@ -1,9 +1,15 @@
 package co.edu.udea.game;
 
+// Importes necesarios para que las pruebas funcionen
 import static org.junit.Assert.*;
 import org.junit.Test;
 
-
+/**
+* Class in charge of testing the Grid class
+* @author Sebastian Montoya Jimenez
+* @version 1.0.0
+* @since 02/09/2018
+*/
 public class GridTest
 {
 
--- test/co/edu/udea/game/TileTest.java
+++ test/co/edu/udea/game/TileTest.java
@@ -1,16 +1,91 @@
 package co.edu.udea.game;
 
+// Importes necesarios para que las pruebas funcionen
 import static org.junit.Assert.*;
 import org.junit.Test;
 
-
+/**
+* Class in charge of testing the Tile class
+* @author Sebastian Montoya Jimenez
+* @version 1.0.0
+* @since 02/09/2018
+*/
 public class TileTest
 {
-
+	
 	@Test
-	public void test()
+	public void testGettingTile() // Prueba para revisar la construccion de una baldosa
 	{
+		Tile baldosa = new Tile();
 		
+		assertEquals(baldosa.getState(), Tile.COVERED_STATE);
+		assertEquals(baldosa.isMarked(), false);
+		assertEquals(baldosa.isMined(), false);
+		assertEquals(baldosa.getSurroundingMines(), 0);
+		// Pasa si la baldosa esta descubierta, sin marca y sin minas
 	}
-
+	
+	@Test
+	public void testGettingAndSettingState() // Prueba para revisar los metodos: getState y setState
+	{
+		Tile baldosa = new Tile();
+		
+		assertEquals(baldosa.getState(), Tile.COVERED_STATE);
+		
+		baldosa.setState(Tile.UNCOVERED_STATE);
+		
+		assertNotEquals(baldosa, Tile.COVERED_STATE);
+		// Pasa si se puede determinar y establecer correctamente el estado de la baldosa
+	}
+	
+	@Test
+	public void testCheckingMark() // Prueba para revisar los metodos: isMarked y setMark
+	{
+		Tile baldosa = new Tile();
+		
+		assertEquals(baldosa.isMarked(), false);
+		
+		baldosa.setMark(true);
+		
+		assertNotEquals(baldosa.isMarked(), false);
+		// Pasa si se puede determinar y establecer correctamente si la baldosa esta marcada o no
+	}
+	
+	@Test
+	public void testCheckingMine() // Prueba para revisar los metodos: isMined y setMine
+	{
+		Tile baldosa = new Tile();
+		
+		assertEquals(baldosa.isMined(), false);
+		
+		baldosa.setMine(true);
+		
+		assertNotEquals(baldosa.isMined(), false);
+		// Pasa si se puede determinar y establecer correctamente si la baldosa contiene una mina o no
+	}
+	
+	
+	@Test
+	public void testGettingSurroundingMines() // Prueba para revisar los metodos: getSurroundingMines y setSurroundingMines
+	{
+		Tile baldosa = new Tile();
+		
+		assertEquals(baldosa.getSurroundingMines(), 0);
+		
+		baldosa.setSurroundingMines(7);
+		
+		assertEquals(baldosa.getSurroundingMines(), 7);
+		// Pasa si se puede determinar y establecer correctamente el numero de minas circundantes en una baldosa
+	}
+	
+	
+	@Test
+	public void testChehckingEmptyTile() // Prueba para revisar el metodo isEmpty
+	{
+		Tile baldosa = new Tile();
+		
+		assertEquals(baldosa.isEmpty(), true);
+		// Pasa si puede dectectar que una baldosa recien creada esta vacia
+	}
+	
 }
